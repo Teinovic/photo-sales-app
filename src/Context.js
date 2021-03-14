15b057fc56bbbd8610b11256ffca3d0ec22ec979
Teinovic <momir.teinovic.2011@gmail.com>
2021-03-14 16:42:35 +0100

    implemented a useEffect hook to load photos to the app

--- src/Context.js
+++ src/Context.js
@@ -1,13 +1,30 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 
 const Context = React.createContext()
 
 function ContextProvider({children}) {
+    const [photoArray, updatePhotoArray] = useState([])
+
+    const photosUrl = 'https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json'
+    
+    useEffect(() => {
+        async function addPhoto() {
+            const photosPromise = await fetch(photosUrl)
+            const photos = await photosPromise.json()
+            updatePhotoArray(photos)
+        }
+        addPhoto()
+      
+
+        }, [])
+    
     return (
-        <Context.Provider value="">
+        <Context.Provider value={{photoArray}}>
             {children}
         </Context.Provider>
     )
 }
 
 export {ContextProvider, Context}
+
+
