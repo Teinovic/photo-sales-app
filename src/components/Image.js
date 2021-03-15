b58efe5e05d9e8d9320c0aa55b9f53fb388d95fc
Teinovic <momir.teinovic.2011@gmail.com>
2021-03-15 11:38:20 +0100

    added onMouseEnter and onMouseLeave event listeners to photos

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,16 @@
-import React from "react"
+import React, {useState} from "react"
 
 function Image({className, img}) {
+    const [isHovered, setIsHovered] = useState(false)
+    
     return (
-        <div className={`${className} image-container`}>
+        <div 
+            className={`${className} image-container`} 
+            onMouseEnter={() => setIsHovered(true)} 
+            onMouseLeave={() => setIsHovered(false)}
+        >
             <img src={img.url} className="image-grid"/>
+
         </div>
     )
 }
